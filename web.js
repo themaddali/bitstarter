9513e1dc4e0fd916a0b6a71a65baba80a17c50fa
Venkat Maddali <maddali@outlook.com>
2013-07-10 17:27:25 -0700

    message of running placed

--- web.js
+++ web.js
@@ -5,4 +5,6 @@ var index = fs.readFileSync('index.html');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end(index);
+  console.log("call heard");
 }).listen(5000);
+console.log("Running on port 5000");
