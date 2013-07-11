d42a02f68faac574f9dc2083a991de12c3a4973b
Venkat Maddali <maddali@outlook.com>
2013-07-10 17:50:18 -0700

    dynamic port for heroku

--- web.js
+++ web.js
@@ -9,5 +9,5 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end(index);
   console.log("call heard");
-}).listen(5000);
-console.log("Running on port 5000");
+}).listen(process.env.PORT || 5000)
+console.log("Running on port 5000 in local env or on heroku");
